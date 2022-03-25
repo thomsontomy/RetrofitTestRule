7dbb069435a74d563880807ea2a739057d77d3e2
Thomson Thomas <thomsontomy@gmail.com>
2022-03-25 20:36:16 +1100

    Publish library

--- testrule/build.gradle.kts
+++ testrule/build.gradle.kts
@@ -15,3 +15,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
+
+mavenPublish {
+    sonatypeHost = com.vanniktech.maven.publish.SonatypeHost.S01
+}
